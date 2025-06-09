8f5e7870f860e43b3a2436521f0c703997f39cc3
Llewelyn Williams <83357820+llewelyn-williams@users.noreply.github.com>
2025-06-09 13:03:53 +0000

    refactor(lang util): Add getLocalisedHomePath.

--- src/lib/utils/lang.ts
+++ src/lib/utils/lang.ts
@@ -2,6 +2,10 @@ export function getLangFromPath(pathname: string): "cy" | "en" {
   return pathname.startsWith("/cy") ? "cy" : "en";
 }
 
+export function getLocalisedHomePath(lang: "en" | "cy"): string {
+  return lang === "cy" ? "/cy" : "/en";
+}
+
 export function getHtmlLangAttribute(pathname: string): string {
   return getLangFromPath(pathname) === "cy" ? "cy" : "en";
 }
